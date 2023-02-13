e35a5c0a96cda7bf1fbf7a990fb3fa7819f94c7f
Has Chanrotanak <haschanrotanak@gmail.com>
2023-02-13 13:19:56 +0530

    FirstJava.java

--- FirstJava.java
+++ FirstJava.java
@@ -1,5 +1,6 @@
 public class FirstJava{
     public static void main(String []arge){
         System.out.println("Hello Banaras Hindu Univesity!!");
+        System.out.println("I'm Rotanak");
     }
 }
